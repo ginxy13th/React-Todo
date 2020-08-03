cd7c4903e2867f6c3e9d0052fafa1951bb62ef17
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-02 23:09:59 -0400

    form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import ToDoForm from './components/TodoForm.js';
+import ToDoList from './components/TodoList.js';
+const tasks = [{
+  task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks,
+    }
+  }
+
+  addTask = taskName => {
+    const newtask = {
+      task: taskName,
+      id: new Date(),
+      completed: false,
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newtask]
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoForm addTask={this.addTask}/>
+        <ToDoList tasks={this.state.tasks}/>
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ToDoForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            task: '',
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            task: e.target.value
+        });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTask(this.state.task)
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type='text'
+                    name='task'
+                    value= {this.state.task}
+                    onChange={this.handleChange} />
+                <button>Add</button>
+            </form>
+        )
+    }
+}
+export default ToDoForm
